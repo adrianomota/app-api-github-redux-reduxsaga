836c0de131f132097df1986e187fd02e9ca10fcd
Adriano Mota <adrianowsh@hotmail.com>
2019-02-19 22:15:37 -0300

    fim do projeto de TODO

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 // configuracao inicial do REDUX
 
 // inicializando redux na aplicacao
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 
 import reducers from './reducers';
 
+const createAppropriateStore = process.env.NODE_ENV === 'development' ? console.tron.createStore : createStore;
+
 // passando os reducers que armazenam e alteram as informacoes de estado de dentro do redux
-const store = createStore(reducers);
+const store = createAppropriateStore(reducers, compose(applyMiddleware(...[])));
 
 export default store;
